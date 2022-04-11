8658a3f632dd84c6b89ca247b272686bcff01fbc
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-04-11 23:48:03 +0200

    Last todo which I'll not implement. :p I'm going to submit this right here.

--- Geehoo/MyGame/EnemyBoss.cs
+++ Geehoo/MyGame/EnemyBoss.cs
@@ -2,15 +2,25 @@
 // Copyright (c) TechnicJelle. All rights reserved.
 // You're allowed to learn from this, but please do not simply copy.
 
+using System;
+
 namespace Geehoo.MyGame;
 
 public class EnemyBoss : Enemy
 {
-	private const int HEALTH = 50;
+	private const int HEALTH = 300;
 
 	public EnemyBoss(float y, float offset) : base(y, offset, 64, HEALTH)
 	{
 	}
 
 	//TODO: Add fancier shooting pattern
+
+	//TODO: Boss follows player
+
+	public override void Recalc()
+	{
+		base.Recalc();
+		Console.WriteLine("Boss health: " + health);
+	}
 }
